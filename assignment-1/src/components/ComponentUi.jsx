b23baed5b36e016080bf55b400bb477b39efe701
jesseannunen <jessean@hotmail.fi>
2024-10-16 03:22:54 +0300

    päivitys

--- assignment-1/src/components/ComponentUi.jsx
+++ assignment-1/src/components/ComponentUi.jsx
@@ -1,40 +1,40 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import OrderForm from "./OrderForm";
 import OrderInfo from "./OrderInfo";
+import Header from "./Header";
 import productImage from "../images/cpu-1.jpg";
 
 
-function ComponentUI () {
-    
+function ComponentUI() {
+
     const products = [
         { name: "AMD Ryzen 5 3600", price: 245 },
         { name: "AMD Ryzen 7 5800 X3D", price: 319 },
         { name: "Intel i9-14900K ", price: 500 },
-    
-      ];
-    
-      const [selectedProduct, setSelectedProduct] = useState(products[0]);
-      const [quantity, setQuantity] = useState(1);
 
-      const total = selectedProduct.price * quantity;
-      
+    ];
 
-      return (
+    const [selectedProduct, setSelectedProduct] = useState(products[0]);
+    const [quantity, setQuantity] = useState(1);
+
+    const total = selectedProduct.price * quantity;
+
+
+    return (
         <div>
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <img src={productImage} alt="Product" style={{ width: '100px', marginRight: '20px' }} />
-            <h1>Welcome to product page!</h1>
-          </div>
-          <OrderForm
-            products={products}
-            selectedProduct={selectedProduct}
-            setSelectedProduct={setSelectedProduct}
-            quantity={quantity}
-            setQuantity={setQuantity}
-          />
-          <OrderInfo selectedProduct={selectedProduct} quantity={quantity} total={total} />
-        </div>
-      );
-    }
-    
-    export default ComponentUI;
+            <div>
+                <Header image={productImage} title="Welcome to the product page!" />
+            </div>
+            <OrderForm
+                products={products}
+                selectedProduct={selectedProduct}
+                setSelectedProduct={setSelectedProduct}
+                quantity={quantity}
+                setQuantity={setQuantity}
+            />
+            <OrderInfo selectedProduct={selectedProduct} quantity={quantity} total={total} />
+        </div >
+    );
+}
+
+export default ComponentUI;
